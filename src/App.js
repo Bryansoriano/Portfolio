20aeaa69794c85db4f4a6b872a74c5f72ef0db3d
bryanS <bryansoriano@knights.ucf.edu>
2022-06-05 15:27:09 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -6,10 +6,14 @@ import About from './components/About'
 import Contact from './components/Contact'
 
 function App() {
+
+
+
+
     return (
         <>
             <Routes>
-                <Route path="/" element={<Layout />}>
+                <Route exact path="/" element={<Layout />}>
                     <Route index element={<Home />} />
                     <Route path="about" element={<About />} />
                     <Route path="contact" element={<Contact />} />
