0a5f4af7bc80464775bd3aef455487974c67cc14
bryanS <bryansoriano@knights.ucf.edu>
2022-06-05 16:15:38 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -4,19 +4,20 @@ import Layout from './components/Layout'
 import Home from './components/Home'
 import About from './components/About'
 import Contact from './components/Contact'
+import { useNavigate } from "react-router-dom"
 
 function App() {
 
-
+    const navigate = useNavigate();
 
 
     return (
         <>
             <Routes>
                 <Route exact path="/" element={<Layout />}>
-                    <Route index element={<Home />} />
-                    <Route path="about" element={<About />} />
-                    <Route path="contact" element={<Contact />} />
+                    <Route exact index element={<Home />} />
+                    <Route exact path="/about" element={<About />} />
+                    <Route exact path="/contact" element={<Contact />} />
                 </Route>
             </Routes>
         </>
